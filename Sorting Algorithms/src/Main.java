81c42e06c50271d65e3407d4308a1e8afaf952da
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-08-04 16:28:19 +0530

    removed swap function and added it to Main.java for optimization

--- Sorting Algorithms/src/Main.java
+++ Sorting Algorithms/src/Main.java
@@ -4,4 +4,12 @@ public class Main {
         BubbleSort ob = new BubbleSort();
         ob.bubbleSort(a);
     }
+
+    public void swap(int[] a, int i, int j) {
+        if (i == j)
+            return;
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 }
